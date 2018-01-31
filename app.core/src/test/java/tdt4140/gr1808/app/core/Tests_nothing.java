6d729bec7135c69c5787ee9566fec2ba35c9faef
Sigurd Aasbrenn <sigurd.aasbrenn@gmail.com>
2018-01-31 11:34:04 +0100

    Adds a test to check if maven testing on gitlab works (tests no actual code)

---
+++ app.core/src/test/java/tdt4140/gr1808/app/core/Tests_nothing.java
@@ -0,0 +1,19 @@
+package tdt4140.gr1808.app.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Tests_nothing {
+
+	@Test
+	public void test1() {
+		assertEquals(1, 1);
+	}
+	
+	@Test
+	public void test2() {
+		assertEquals(1, 1);
+	}
+
+}
