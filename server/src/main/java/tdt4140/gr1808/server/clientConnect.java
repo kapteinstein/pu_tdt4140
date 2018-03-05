87dac131ab35fd1ac5069b7ef8587481afc7df7a
maritoye <maritg@me.com>
2018-03-05 10:54:57 +0100

    #14 Minor fixes + making future issues easier to implement
    Changes @eriklio did: change main to a run() and did some small fixes
    there, as well as some minor fixes elsewhere.
    
    Began to add functionality to make it easier to implement later issues.
    This is currently commented out, as it isn't being tested yet.

--- server/src/main/java/tdt4140/gr1808/server/clientConnect.java
+++ server/src/main/java/tdt4140/gr1808/server/clientConnect.java
@@ -1,84 +1,120 @@
 package tdt4140.gr1808.server;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
+import java.io.*;
 import java.net.Socket;
-import java.net.SocketException;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-public class clientConnect { 
+import java.util.HashMap;
+
+public class ClientConnect extends Thread {
+	private DBQuery dbquery;
+	private ServerParser parser;
+	private BufferedReader inputStream;
+	private DataOutputStream outputStream;
+	private Socket connectionSocket;
 	
-	private static DBQuery dbquery;
-	private static ServerParser parser;
-	private static BufferedReader inputStream;
-	private static DataOutputStream outputStream;
-	private static Socket connectionSocket;
+	public ClientConnect(DBQuery dbquery,
+		     ServerParser parser,
+		     InputStream inputStream,
+		     OutputStream outputStream,
+		     Socket connectionSocket) {
+	this.dbquery = dbquery;
+	this.parser = parser;
+	this.inputStream = new BufferedReader(new InputStreamReader(inputStream));
+	this.outputStream = new DataOutputStream(outputStream);
+	this.connectionSocket = connectionSocket;
+}
 	
-	
-	public clientConnect(DBQuery dbq, Parser parser, BufferedReader inputStream, DataOutputStream outputstream, Socket connectionSocket) {
-		this.dbquery = dbq;
-		this.parser = parser;
-		this.inputStream = inputStream;
-		this.outputStream = outputstream;
-		this.connectionSocket = connectionSocket;
-	}
-	
-	
-	private static void action(JSONObject jsonData) {
+	private void action(JSONObject jsonData) {
 		int userId;
 		String mode;
-		
-		String parsedData = parser.decode(jsonData);
-		
-		//skal sjekke om brukeren er av rett type til å få lov å gjøre denne operasjonen
-		//userId = parsedData.getString("user_id");
-		//String userType = DBQuery.get_user_type(userId)
-				
-		//String mode = parsedData.getString("mode");
-		
-		//sender dataen til metoden som skal kalle rette metoder i DBQuery
-		connect(parsedData);
-		
-	}
-	
-	private static void connect(String parsedData) {
-		//FORMÅL: få modus, user etc fra en bruker og utføre kommandoen. 
-		//Dersom man får returnert data, skal denne sendes på outputStream (kan evt gjøres i egen metode)
-		
-		//denne trenger ikke sjekke om brukeren får lov å gjøre denne operasjonen, har allerede sjekket
-		
-		//parsedData er HashMap
+		String userType;
 
-		//bruk parser til å stringifye dataen
-		//får dataen tilbake på denne formen: array?
+		HashMap<String, String> parsedData = parser.decode(jsonData); 
+		/*
+		//nesten alt dette tilhører senere issues og er derfor kommentert ut for oyeblikket siden det ikke skal testes enda.
+		//skal sjekke om brukeren er av rett type til a fa lov a gjore denne operasjonen og om dataen er på rett format osv
+
+		userId = parsedData.get("user_id");
+		try {
+			userType = dbquery.get_user_type(userId);
+		}
+		finally{
+			//sende tilbakemelding om at brukeren ikke finnes
+		}
 		
-		//String mode = parsedData.getString("mode");
-		//kall rett metode i dbquery med en for-løkke for å sende inn/hente data
-		
-	}
-	
-	public static void main(String[] args) {
-		
-		while(true){	
-			try {
-				JSONObject jsonData;
-				String line;
-				StringBuilder sb = new StringBuilder();
-				while ((line = inputStream.readLine()) != null) {
-					sb.append(line);
+		switch(parsedData.get("mode")) {
+			case "add_user":
+				//skal senere sjekke om dataene er på rett form
+				connect(parsedData);	
+				break;
+			
+			case "delete_user":
+				if (userType == "datagiver"){
+					//sjekke om brukeren ikke prover a slette noen andre enn seg selv
+					connect(parsedData);
+				} else {
+					//send en melding tilbake om at denne brukeren ikke far lov a gjore dette?
+					break;
 				}
-				jsonData = new JSONObject(sb.toString());
-				action(jsonData);
+			
+			case "add_data":
+				if (userType == ""){
+					connect(parsedData);
+				} else {
+					//send en melding tilbake om at denne brueren ikke far lov a gjore dette?
+					break;
+				}
+			case "get_data":
+				if (userType == "Tjenesteyter"){ 
+					//her kan vi senere legge inn sjekker for a se om dataene er på rett form
+					//Skal senere sjekke her om brukeren som vi prøver aa se dataene til har gitt tillatelse for dette!
+					connect(parsedData);
+				} else if (userType == "Datagiver"){ 
+					//kan senere legge inn sjekker for å se om dataene er på rett form
+					//Sjekke at datagiveren kun prøver å se sine egne data. Viss ikke skal de ikke ha tilgang.
+					connect(parsedData);
+				} break;
 				
-			} catch(SocketException e){
-				e.printStackTrace();;
-			} finally{
-				inputStream.close();
-				connectionSocket.close();
+			case "response":
+				if (userType == ""){
+					connect(parsedData);
+				} else {
+					//send en melding tilbake om at denne brukeren ikke far lov a gjore dette?
+					break;
+				}
+			default:
+				//tilbakemelding om at det var ugylding mode
+		}
+		*/
+		connect(parsedData); //denne vil bli tatt vekk nar det over blir ferdig
+	}
+
+	private void connect(HashMap<String, String> parsedData) {
+
+	}
+
+	@Override
+	public void run() {
+		try {
+			JSONObject jsonData;
+			String line;
+			StringBuilder sb = new StringBuilder();
+			while ((line = inputStream.readLine()) != null) {
+				sb.append(line);
+			}
+			jsonData = new JSONObject(sb.toString());
+			action(jsonData);
+
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (!connectionSocket.isClosed()) {
+				try {
+					connectionSocket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
