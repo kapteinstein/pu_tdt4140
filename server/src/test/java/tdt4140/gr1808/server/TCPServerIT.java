353b456b20f9ca82c38d43730286581401904260
Erik Liodden <eriklio@stud.ntnu.no>
2018-03-07 17:24:28 +0100

    tests: add dummy client and interaction tests
    test connections to the server with a tiny dummy client.
    relates to issue #18

--- server/src/test/java/tdt4140/gr1808/server/TCPServerIT.java
+++ server/src/test/java/tdt4140/gr1808/server/TCPServerIT.java
@@ -4,11 +4,41 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.Socket;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
+class TCPClient {
+	Socket connection = null;
+	DataOutputStream out;
+	BufferedReader in;
+
+	TCPClient(int port) throws IOException {
+		connection = new Socket("localhost", port);
+	}
+
+	public String connectStatus() {
+		return String.valueOf(connection.getRemoteSocketAddress());
+	}
+
+	public Socket getConnection() {
+		return connection;
+	}
+
+	public void generateIn() throws IOException {
+		in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	}
+
+	public void generateOut() throws IOException {
+		out = new DataOutputStream(connection.getOutputStream());
+	}
+
+	public void write(String data) throws IOException {
+		out.writeUTF(data);
+		System.out.println("data written: " + data);
+	}
+}
 
 public class TCPServerIT {
 	TCPServer server = null;
@@ -36,6 +66,99 @@ public class TCPServerIT {
 	public void runTest() {
 		server.start();
 		assertEquals(0, server.close());
+	}
+
+	@Test
+	public void clientConnectSuccessTest() {
+		int port = 6789;
+		TCPClient client = null;
+
+		try {
+			client = new TCPClient(port);
+			assertTrue(client.getConnection() != null);
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
+
+	}
+
+	@Test
+	public void clientConnectFailTest() {
+		int port = 6789;
+		TCPClient client = null;
+		server.close();
+		// the server is now closed
+		try {
+			client = new TCPClient(port);
+			fail();
+		} catch (IOException e) {
+		}
+	}
+
+	@Test
+	public void clientInputSteamTest() {
+		int port = 6789;
+		TCPClient client = null;
+		try {
+			client = new TCPClient(port);
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
+
+		try {
+			client.generateIn();
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	@Test
+	public void clientOutputSteamTest() {
+		int port = 6789;
+		TCPClient client = null;
+		try {
+			client = new TCPClient(port);
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
+		try {
+			client.generateOut();
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	@Test
+	public void clientServerInteractionTest() {
+		int port = 6789;
+		String data = "test data";
+		TCPClient client = null;
+		server.start();
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			client = new TCPClient(port);
+			client.generateIn();
+			client.generateOut();
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
+		try {
+			client.write(data);
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
 	}
 
 	@After
