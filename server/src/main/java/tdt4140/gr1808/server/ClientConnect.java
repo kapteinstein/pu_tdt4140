36a41fa0463fe39d8f92a65f5c9c92a50e692336
Sigurd Aasbrenn <sigurd.aasbrenn@gmail.com>
2018-03-14 14:39:33 +0100

    #29 Added responses from the server to other actions than 'get...' and did some cleaning up in the Client connect class and the corresponding test file

--- server/src/main/java/tdt4140/gr1808/server/ClientConnect.java
+++ server/src/main/java/tdt4140/gr1808/server/ClientConnect.java
@@ -1,10 +1,8 @@
 package tdt4140.gr1808.server;
 
 import java.io.*;
-import java.net.Socket;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -18,138 +16,6 @@ public class ClientConnect extends Thread {
 		this.parser = parser;
 		this.connection = connection;
 	}
-
-	private void action(JSONObject jsonData) {
-		int userId;
-		String mode;
-		String userType;
-
-		HashMap<String, String> parsedData = parser.decode(jsonData);
-		/*
-		 * //nesten alt dette tilhører senere issues og er derfor kommentert ut for
-		 * oyeblikket siden det ikke skal testes enda. //skal sjekke om brukeren er av
-		 * rett type til a fa lov a gjore denne operasjonen og om dataen er på rett
-		 * format osv
-		 * 
-		 * userId = parsedData.get("user_id"); try { userType =
-		 * dbquery.get_user_type(userId); } finally{ //sende tilbakemelding om at
-		 * brukeren ikke finnes }
-		 * 
-		 * switch(parsedData.get("mode")) { case "add_user": //skal senere sjekke om
-		 * dataene er på rett form connect(parsedData); break;
-		 * 
-		 * case "delete_user": if (userType == "datagiver"){ //sjekke om brukeren ikke
-		 * prover a slette noen andre enn seg selv connect(parsedData); } else { //send
-		 * en melding tilbake om at denne brukeren ikke far lov a gjore dette? break; }
-		 * 
-		 * case "add_data": if (userType == ""){ connect(parsedData); } else { //send en
-		 * melding tilbake om at denne brueren ikke far lov a gjore dette? break; } case
-		 * "get_data": if (userType == "Tjenesteyter"){ //her kan vi senere legge inn
-		 * sjekker for a se om dataene er på rett form //Skal senere sjekke her om
-		 * brukeren som vi prøver aa se dataene til har gitt tillatelse for dette!
-		 * connect(parsedData); } else if (userType == "Datagiver"){ //kan senere legge
-		 * inn sjekker for å se om dataene er på rett form //Sjekke at datagiveren kun
-		 * prøver å se sine egne data. Viss ikke skal de ikke ha tilgang.
-		 * connect(parsedData); } break;
-		 * 
-		 * case "response": if (userType == ""){ connect(parsedData); } else { //send en
-		 * melding tilbake om at denne brukeren ikke far lov a gjore dette? break; }
-		 * default: //tilbakemelding om at det var ugylding mode }
-		 */
-		try {
-			connect(parsedData); // denne vil bli tatt vekk nar det over blir ferdig
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void connect(HashMap<String, String> parsedData) throws Exception {
-		String stringData = "";
-		HashMap<String, String> hm;
-		// FORMÅL: få modus, user etc fra en bruker og utføre kommandoen.
-		// Dersom man får returnert data, skal denne sendes på outputStream (kan evt
-		// gjøres i egen metode)
-		// DBQuery query = new DBQuery(); // ikke ha med
-
-		switch (parsedData.get("mode")) {
-		case "add_user":
-			hm = dbquery.addUser(parsedData.get("user_type"), parsedData.get("name"));
-			stringData = hm.get("message");
-			break;
-		case "delete_user":
-			hm = dbquery.deleteUser(parsedData.get("user_id"));
-			stringData = hm.get("message");
-			break;
-		case "add_data":
-			ServerParser serverParser = new ServerParser();
-			List<Touple> arrayList = serverParser.deStringify(parsedData.get("data"));
-
-			for (int i = 0; i < arrayList.size(); i++) {
-				hm = dbquery.addPulseData(parsedData.get("user_id"), parsedData.get("data_type"),
-						arrayList.get(i).getPuls(), arrayList.get(i).getDateTime());
-				stringData += hm.get("message") + "\n";
-			}
-			break;
-		case "delete_data":
-			hm = dbquery.deletePulseData(parsedData.get("user_id"), parsedData.get("data_type"),
-					parsedData.get("start_datetime"), parsedData.get("end_datetime"));
-			stringData = hm.get("message");
-			break;
-		case "get_data":
-			hm = dbquery.getPulseData(parsedData.get("user_id"), parsedData.get("data_type"),
-					parsedData.get("start_datetime"), parsedData.get("end_datetime"));
-			// String stringData = dbquery.getPulseData(parsedData.get("user_id"),
-			// parsedData.get("data_type"),
-			// parsedData.get("start_datetime"), parsedData.get("end_datetime"));
-			if (hm.get("status").equals("success")) {
-				stringData = hm.get("message") + "\n" + hm.get("data");
-			} else {
-				stringData = hm.get("message");
-			}
-			break;
-		case "response":
-			JSONObject data = parser.encode(parsedData);
-			try {
-				connection.write(data);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			break;
-		// legger HashMap ut på outputStream
-		default:
-			throw new Exception(
-					"This should never happen. Mode was: " + parsedData.get("mode") + parsedData.get("data"));
-		}
-		try {
-			System.out.println(stringData);
-			connection.write(new JSONObject(stringData));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} // Legger dataen som skal til tjenesteyter ut på outputStreamen
-			// denne trenger ikke sjekke om brukeren får lov å gjøre denne operasjonen, har
-			// allerede sjekket
-
-		// parsedData er HashMap
-
-		// bruk parser til å stringifye dataen
-		// får dataen tilbake på denne formen: array?
-
-		// String mode = parsedData.getString("mode");
-		// kall rett metode i dbquery med en for-løkke for å sende inn/hente data
-	}
-
-	/*public JSONObject inputToJson(BufferedReader input) throws IOException {
-		JSONObject jsonData = new JSONObject();
-		String line;
-		StringBuilder sb = new StringBuilder();
-		while ((line = input.readLine()) != null) {
-			sb.append(line);
-		}
-		jsonData = new JSONObject(sb.toString());
-		return jsonData;
-	}*/
 
 	@Override
 	public void run() {
@@ -167,5 +33,67 @@ public class ClientConnect extends Thread {
 				}
 			}
 		}
+	}
+	
+	private void action(JSONObject jsonData) {
+		HashMap<String, String> parsedData = parser.decode(jsonData);
+		try {
+			connect(parsedData);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void connect(HashMap<String, String> parsedData) throws Exception {
+		HashMap<String, String> hm = new HashMap<String, String>();
+
+		switch (parsedData.get("mode")) {
+		
+		case "add_user":
+			hm = dbquery.addUser(parsedData.get("user_type"), parsedData.get("name"));
+			break;
+		
+		case "delete_user":
+			hm = dbquery.deleteUser(parsedData.get("user_id"));
+			break;
+		
+		case "add_data":
+			List<Touple> arrayList = parser.deStringify(parsedData.get("data"));
+
+			for (int i = 0; i < arrayList.size(); i++) {
+				hm = dbquery.addPulseData(parsedData.get("user_id"), parsedData.get("data_type"),
+						arrayList.get(i).getPuls(), arrayList.get(i).getDateTime());
+			}
+			break;
+		
+		case "delete_data":
+			hm = dbquery.deletePulseData(parsedData.get("user_id"), parsedData.get("data_type"),
+					parsedData.get("start_datetime"), parsedData.get("end_datetime"));
+			break;
+		
+		case "get_data":
+			hm = dbquery.getPulseData(parsedData.get("user_id"), parsedData.get("data_type"),
+					parsedData.get("start_datetime"), parsedData.get("end_datetime"));
+			break;
+		
+		case "response":
+			JSONObject data = parser.encode(parsedData);
+			try {
+				connection.write(data);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			break;
+		default:
+			throw new Exception(
+					"This should never happen. Mode was: " + parsedData.get("mode") + parsedData.get("data"));
+		}
+		try {
+			if (!hm.isEmpty()) {
+				connection.write(parser.encode(hm));
+			}	
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
 	}
 }
--- server/src/test/java/tdt4140/gr1808/server/ClientConnectTest.java
+++ server/src/test/java/tdt4140/gr1808/server/ClientConnectTest.java
@@ -14,54 +14,6 @@ import org.junit.Before;
 import org.json.JSONObject;
 
 public class ClientConnectTest {
-	
-	/*
-	@InjectMocks
-	ClientConnect c1,c2;
-	
-	@Mock
-	DBQuery q;
-	ServerParser p;
-	BufferedReader b1,b2;
-	Socket s;
-	DataOutputStream d;
-
-		@Test
-		public void inputToJsonTest() throws IOException{
-			Exception error;
-			String s1, s2;
-			String test1, test2;
-			String testString1,testString2;
-			InputStream inputStream1, inputStream2;
-			
-			
-			//first			
-			test1 = new JSONObject().put("mode", "delete_user").toString();
-			inputStream1 = new ByteArrayInputStream(test1.getBytes());
-			b1 = new BufferedReader(new InputStreamReader(inputStream1));
-			c1 = new ClientConnect(q,p,b1,d,s);
-
-			testString1 = new JSONObject().put("mode", "delete_user").toString();
-			s1 = c1.inputToJson(b1).toString();
-			
-			assertEquals(testString1,s1);
-			
-			
-			//second
-			test2 = "illegal argument";
-			inputStream2 = new ByteArrayInputStream(test2.getBytes());
-			b2 = new BufferedReader(new InputStreamReader(inputStream2));
-			c2 = new ClientConnect(q,p,b2,d,s);
-
-			testString2 = "org.json.JSONException: A JSONObject text must begin with '{' at 1 [character 2 line 1]";
-			try{
-				s2 = c1.inputToJson(b2).toString();		
-			}catch (Exception e) {
-				error = e;	
-				s2 = error.toString();
-			}
-			assertEquals(testString2,s2);
-	}*/
 		
 		private DBQuery dbQuery;
 		private ServerParser serverParser;
